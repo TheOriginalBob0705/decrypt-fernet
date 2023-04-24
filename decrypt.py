4b4800898ab12b0bff2893fce13b1206f3f526a6
brendan.craven <brendan.craven@student.howest.be>
2023-04-24 14:56:33 +0200

    read a key file, take an encrypted file and decrypt it

---
+++ decrypt.py
@@ -0,0 +1,14 @@
+from cryptography.fernet import Fernet
+
+with open("key.key","rb") as f:
+    key = f.read()
+f = Fernet(key)
+
+with open('encrypted_file.txt', 'rb') as encrypted_file:
+    encrypted = encrypted_file.read()
+
+decrypted = f.decrypt(encrypted)
+
+with open('decrypted_file.txt', 'wb') as decrypted_file:
+    decrypted_file.write(decrypted)
+
